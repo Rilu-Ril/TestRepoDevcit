b49094ce66fac5bff409df843af4ad604034a382
Baha Ganyev <marvel@MacBook-Pro-Baha.local>
2018-07-26 20:49:14 +0600

    Done

--- Github/Github/RegistrationViewController.swift
+++ Github/Github/RegistrationViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class RegistrationViewController: UIViewController {
 
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var user: UITextField!
+    @IBOutlet weak var pass: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var forgetButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "Долина реки.jpg")!)
        
     }
 
